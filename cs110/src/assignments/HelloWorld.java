1ce7dc9940346918b823b6f5e888a68bd4aad5d5
rgordon <rgordon@smumn.edu>
2013-01-15 19:34:24 -0600

    changed a line

--- cs110/src/assignments/HelloWorld.java
+++ cs110/src/assignments/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println(" a new line");
 	}
 	
 
