18ea668272a0e5fe42af202f16b1b79ed84f856d
rgordon <rgordon@smumn.edu>
2013-01-15 19:21:10 -0600

    initial commit

---
+++ cs110/src/assignments/HelloWorld.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class HelloWorld {
+	public static void main(String[] arg) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+	
+
+}
