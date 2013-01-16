116826f06077ba6cda4f706f1510083f1b4dee5c
rgordon <rgordon@smumn.edu>
2013-01-16 07:32:22 -0600

    comment

--- cs110/src/assignments/HelloWorld.java
+++ cs110/src/assignments/HelloWorld.java
@@ -9,6 +9,9 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println(" a newer line");
+		
+		// another comment Wed am
+		// a comment
 	}
 	
 
